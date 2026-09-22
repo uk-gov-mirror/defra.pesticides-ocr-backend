f4f83b02c85acb052a97deac1c6598413e135a65
Saranya <Saranya.Vinayagam@cognizant.com>
2026-09-22 12:07:03 +0100

    fix sonar issue

--- src/common/constants/common-constants.js
+++ src/common/constants/common-constants.js
@@ -1,4 +1,4 @@
 // common constants that are used across the application and should not be duplicated as magic strings in multiple places.
 
-export const MAGIC_NO_SIX = '6'
-export const MAGIC_NO_FOUR = '4'
+export const MAGIC_NO_SIX = 6
+export const MAGIC_NO_FOUR = 4
